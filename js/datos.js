39881a6262aedfee6166278c4f85c07683d0443d
Felipe Urrutia <thewomins@gmail.com>
2022-06-29 23:50:18 -0400

    change datos to another file and added proyectos view

---
+++ js/datos.js
@@ -0,0 +1,27 @@
+const datos=[
+    {
+        nombre_proyecto : "Administración crypto",
+        img : "images/captura_prog1.JPG",
+        descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. realizado en <b>Python</b> y su interfaz grafica con la libreria <b>Tkinter</b>",
+        lenguajes:["Python","Tkinter"],
+        link: {"Git Hub":"https://github.com/thewomins/Administrar-crypto-"}
+    },
+    {
+        nombre_proyecto : "Promedio ponderado",
+        img : "images/captura_prog2.jpg",
+        descripcion: "Programa para calcular el promedio ponderado de multiples calificaciones. desarrollado para dispositivos Android utilizando <b>Java</b>. Disponible en Google play store",
+        lenguajes:["Java"],
+        link: {"Git Hub":"https://github.com/thewomins/promedios",
+                "Google play store":"https://play.google.com/store/apps/details?id=com.ominscorp.promedios"
+            }
+    },
+    {
+        nombre_proyecto : "Sistema compra pasajes para trenes",
+        img : "images/captura_prog1.JPG",
+        lenguajes:["Python","Fast API","JavaScript","MongoDB"],
+        descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. realizado en <b>Python</b> y su interfaz grafica con la libreria <b>Tkinter</b>",
+        link: {"Git Hub":"https://github.com/thewomins/Administrar-crypto-"}
+    }
+];
+
+export {datos}
--- js/script.js
+++ js/script.js
@@ -1,88 +1,41 @@
+import { datos } from "./datos.js";
+
+const template=`
+<img></img>
+<div class="div-datos-proyectos">
+    <h3 style="align-self:flex-end">nombre proyecto</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas officiis nisi similique corrupti eum magni deserunt. Quos aut enim necessitatibus veritatis, fugiat officia, maiores vero repellat ut laudantium atque repudiandae.</p>
+    <div class="lenguajes">
+    </div>
+</div>
+`
+
 document.getElementById("contenedor-arrow").addEventListener("click",e=>onclick_arrow())
 
 function onclick_arrow(){
     console.log("cicku",screen.availHeight)
-    window.scrollBy(0, window.innerHeight-65);
+    window.scrollBy(0, window.innerHeight-90);
 }
-
-function cerrar() {
-    console.log("clickeaste X");
-    var modal = document.getElementById("miModal");
-    console.log("clickeaste X");
-    modal.style.display = "none";
-}
+iniciar_proyectos()
+function iniciar_proyectos(){
+    let container_proyectos = document.getElementById("lista_proyectos")
+    datos.forEach((element,i)=>{
+        let container_interior = document.createElement("div")
+        container_interior.id=i
+        container_interior.className="container_interior_proyectos"
+        container_interior.innerHTML=template
+        container_interior.querySelector("h3").textContent = element.nombre_proyecto
+        container_interior.querySelector("p").innerHTML = element.descripcion
+        container_interior.querySelector("img").src = element.img
+        let container_lenguajes = container_interior.querySelectorAll("div")[1]
+        container_interior.className="container_interior_proyectos"
+        element.lenguajes.forEach(lenguaje=>{
+            let lenguaje_p=document.createElement("p")
+            lenguaje_p.innerHTML=lenguaje
+            container_lenguajes.appendChild(lenguaje_p)
+        })
+        console.log(i,element)
+        container_proyectos.appendChild(container_interior)
+    })
     
-function modal(pos) {
-    var modal = document.getElementById("miModal");
-    var data = datos();
-    console.log(modal.querySelector("h1"));
-    modal.querySelector("h1").innerHTML = data[pos].h1;
-    modal.querySelector("img").setAttribute("src",data[pos].img);
-    modal.querySelector("p").innerHTML = data[pos].p;
-    var a = modal.querySelector("a");
-    
-    a.innerHTML = data[pos].a;
-    a.setAttribute("href",data[pos].link);
-    a.setAttribute("target","_blank");
-    a.setAttribute("rel","noopener noreferrer");
-    a.style.display = "block";
-    var a2 = modal.querySelectorAll("a")[1];
-    if(Object.keys(data[pos]).length>5){
-        console.log("mamamia");
-        console.log(a2);
-        a2.innerHTML = data[pos].b;
-        a2.style.display = "block";
-        a2.setAttribute("href",data[pos].link2);
-        a2.setAttribute("target","_blank");
-        a2.setAttribute("rel","noopener noreferrer");
-    }
-    else{
-        a2.innerHTML = "";
-        a2.style.display = "none";
-    }
-    modal.style.transition = "display 2s";
-    modal.style.display = "block";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("miModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-function datos(){
-    modal_content=[
-        {
-            h1 : "Administración crypto",
-            img : "images/captura_prog1.JPG",
-            p: "Programa realizado para tener control sobre las inversiones en crypto monedas. realizado en <b>Python</b> y su interfaz grafica con la libreria <b>Tkinter</b>",
-            a: "Git Hub",
-            link: "https://github.com/thewomins/Administrar-crypto-"
-        },
-        {
-            h1 : "Promedio ponderado",
-            img : "images/captura_prog2.jpg",
-            p: "Programa para calcular el promedio ponderado de multiples calificaciones. desarrollado para dispositivos Android utilizando <b>Java</b>. Disponible en Google play store",
-            a: "Git Hub",
-            b: "Google play store",
-            link: "https://github.com/thewomins/promedios",
-            link2: "https://play.google.com/store/apps/details?id=com.ominscorp.promedios"
-        },
-        {
-            h1 : "Portafolio",
-            img : "images/captura_prog3.jpg",
-            p: "primer programa",
-            a: "Git Hub",
-            link: "https://thewomins.github.io"
-        },
-        {
-            h1 : "Sistema escuela",
-            img : "images/captura_prog4.jpg",
-            p: "Programa para almacenar alumnos y asignarles ramos y notas a estos. desarrollado con <b>C++</b>",
-            a: "Git Hub",
-            link: "https://github.com/thewomins/sistema_escuela"
-        },
-    ];
-    return modal_content;
 }
