4d25de20d53f0bf63b3de617d91427c3cbd07168
Felipe Urrutia <thewomins@gmail.com>
2022-07-02 12:42:26 -0400

    v2 end

--- js/datos.js
+++ js/datos.js
@@ -2,14 +2,14 @@ const datos=[
     {
         nombre_proyecto : "Administración crypto",
         img : "images/captura_prog1.JPG",
-        descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. realizado en <b>Python</b> y su interfaz grafica con la libreria <b>Tkinter</b>",
+        descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. leyendo desde una API publica los valores de cada moneda",
         lenguajes:["Python","Tkinter"],
         link: {"Git Hub":"https://github.com/thewomins/Administrar-crypto-"}
     },
     {
         nombre_proyecto : "Promedio ponderado",
         img : "images/captura_prog2.jpg",
-        descripcion: "Programa para calcular el promedio ponderado de multiples calificaciones. desarrollado para dispositivos Android utilizando <b>Java</b>. Disponible en Google play store",
+        descripcion: "Programa para calcular el promedio ponderado de multiples calificaciones. Disponible en Google play store",
         lenguajes:["Java"],
         link: {"Git Hub":"https://github.com/thewomins/promedios",
                 "Google play store":"https://play.google.com/store/apps/details?id=com.ominscorp.promedios"
@@ -19,8 +19,9 @@ const datos=[
         nombre_proyecto : "Sistema compra pasajes para trenes",
         img : "images/captura_prog1.JPG",
         lenguajes:["Python","Fast API","JavaScript","MongoDB"],
-        descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. realizado en <b>Python</b> y su interfaz grafica con la libreria <b>Tkinter</b>",
-        link: {"Git Hub":"https://github.com/thewomins/Administrar-crypto-"}
+        descripcion: "Programa con una interfaz de usuario y administrador en la cual se pueden ingresar los datos de los recorridos a una base de datos de mongoDB a travez de una <b>API Rest</b> y en la cual un usuario puede comprar un pasaje en estos recorridos",
+        link: {"Git Hub Frontend":"https://github.com/thewomins/Frontend_tren",
+                "Git Hub Backend":"https://github.com/thewomins/Backend_tren"}
     }
 ];
 
