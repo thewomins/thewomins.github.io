dbe77995b88b99ab7c7fe0bab75dfcb67ee05a83
Felipe Urrutia <thewomins@gmail.com>
2022-07-04 15:32:56 -0400

    now show repos links

--- js/datos.js
+++ js/datos.js
@@ -4,24 +4,28 @@ const datos=[
         img : "images/captura_prog1.JPG",
         descripcion: "Programa realizado para tener control sobre las inversiones en crypto monedas. leyendo desde una API publica los valores de cada moneda",
         lenguajes:["Python","Tkinter"],
-        link: {"Git Hub":"https://github.com/thewomins/Administrar-crypto-"}
+        link: [{"Git Hub":"https://github.com/thewomins/Administrar-crypto-",
+                "icon":"images/icon_github.svg"}]
     },
     {
         nombre_proyecto : "Promedio ponderado",
         img : "images/captura_prog2.jpg",
         descripcion: "Programa para calcular el promedio ponderado de multiples calificaciones. Disponible en Google play store",
         lenguajes:["Java"],
-        link: {"Git Hub":"https://github.com/thewomins/promedios",
-                "Google play store":"https://play.google.com/store/apps/details?id=com.ominscorp.promedios"
-            }
+        link: [{"Git Hub":"https://github.com/thewomins/promedios",
+                "icon":"images/icon_github.svg"},
+                {"Google play store":"https://play.google.com/store/apps/details?id=com.ominscorp.promedios",
+                "icon":"images/icon_github.svg"}]
     },
     {
         nombre_proyecto : "Sistema compra pasajes para trenes",
         img : "images/tren.png",
         lenguajes:["Python","Fast API","JavaScript","MongoDB"],
         descripcion: "Programa con una interfaz de usuario y administrador en la cual se pueden ingresar los datos de los recorridos a una base de datos de mongoDB a travez de una <b>API Rest</b> y en la cual un usuario puede comprar un pasaje en estos recorridos",
-        link: {"Git Hub Frontend":"https://github.com/thewomins/Frontend_tren",
-                "Git Hub Backend":"https://github.com/thewomins/Backend_tren"}
+        link: [{"Git Hub Frontend":"https://github.com/thewomins/Frontend_tren",
+                "icon":"images/icon_github.svg"},
+                {"Git Hub Backend":"https://www.github.com/thewomins/Backend_tren",
+                "icon":"images/icon_github.svg"}]
     }
 ];
 
--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ function iniciar_proyectos(){
         container_interior.querySelector("h3").textContent = element.nombre_proyecto
         container_interior.querySelector("p").innerHTML = element.descripcion
         container_interior.querySelector("img").src = element.img
+        container_interior.addEventListener("click",e=>onclick_proyecto(container_interior))
         let container_lenguajes = container_interior.querySelectorAll("div")[1]
         container_interior.className="container_interior_proyectos"
         element.lenguajes.forEach(lenguaje=>{
@@ -34,8 +35,45 @@ function iniciar_proyectos(){
             lenguaje_p.innerHTML=lenguaje
             container_lenguajes.appendChild(lenguaje_p)
         })
-        console.log(i,element)
+        
         container_proyectos.appendChild(container_interior)
     })
-    
+}
+
+function onclick_proyecto(element){
+    let div_proyecto=element.querySelector("div")
+    let proyecto_name = div_proyecto.querySelector("h3").textContent
+    if(element.lastChild.id===proyecto_name){
+        console.warn("a")
+        div_proyecto.style="display:flex"
+        element.querySelector("img").style="display:flex"
+        element.removeChild(element.lastChild)
+        return
+    }
+    div_proyecto.style="display:none"
+    element.querySelector("img").style="display:none"
+    let container_links = document.createElement("div")
+    container_links.className="container-links"
+    container_links.id=proyecto_name
+    let proyect = datos.find(e=>e.nombre_proyecto===proyecto_name)
+    //console.warn(proyect)
+    let container_boton = document.createElement("div")
+    container_boton.style="display:flex;width:fit-content; max-width;100%"
+    proyect.link.forEach((e,i)=>{
+        console.warn( typeof(Object.entries(e)[0][1]))
+        let button = document.createElement("div")
+        let img = document.createElement("img")
+        let texto = document.createElement("p")
+        button.className="container-boton-links"
+        button.addEventListener("click",e=>window.location.href = (Object.entries(e)[0][1]))
+        console.error(e)
+        img.src=Object.entries(e)[1][1]
+        texto.textContent=Object.keys(e)[0]
+        button.appendChild(img)
+        button.appendChild(texto)
+        container_boton.appendChild(button)        
+        console.log(button)
+    })
+    container_links.appendChild(container_boton)
+    element.appendChild(container_links)
 }
