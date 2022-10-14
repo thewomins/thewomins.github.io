ada6055de4031d9230cdae02eeeff240c5bcb6c3
Felipe Urrutia <thewomins@gmail.com>
2022-10-14 12:07:39 -0300

    Fix format

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Menu from "./Components/Menu";
 import Presentation from "./Screens/Presentation";
+import About from "./Screens/About";
 
 function App() {
   return (
     <div>
-      <Menu/>
-      <Presentation/>
+      <Menu />
+      <Presentation />
+      <About />
     </div>
   );
 }
