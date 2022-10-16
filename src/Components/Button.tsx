ec7522c570c7800b927694fa47e85bfb0a93e2c8
Felipe Urrutia <thewomins@gmail.com>
2022-10-16 17:16:43 -0300

    Add button component

---
+++ src/Components/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+type props = {
+  primary?: boolean;
+  icon?:any
+  text: string;
+  onClick?: any;
+};
+
+const Button: React.FC<props> = ({primary = false, onClick, text,icon}) => {
+  return (
+    <div
+      className={`${styles.button} ${primary ? styles.primary : ""}`}
+      onClick={() => onClick()}
+    >
+      <div className={styles.image}>{icon}</div>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default Button;
