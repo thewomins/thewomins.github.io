5c4078157fb5f4f7fb78ce4436661a1393d736e0
Felipe Urrutia <thewomins@gmail.com>
2022-10-17 19:18:48 -0300

    Completed menu

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,11 +1,30 @@
+import React, {useRef} from "react";
 import styles from "./Menu.module.css";
+import {MdModeNight} from "react-icons/md";
+import Switch from "./Switch";
+import ButtonMenu from "./ButtonMenu";
+import {TScreens} from "src/types/types";
 
-function Menu() {
+type props = {
+  state: boolean;
+  onChangeCheck: any;
+  screens: TScreens[];
+};
+
+const Menu: React.FC<props> = ({state, onChangeCheck, screens}) => {
   return (
     <div className={styles.menu}>
-      <p>menu</p>
+      <div className={styles.buttonsContainer}>
+        {screens.map((key, i) => (
+          <ButtonMenu key={i} text={key} to={key} />
+        ))}
+      </div>
+      <div className={styles.nightModeContainer}>
+        <MdModeNight className={styles.iconNight} />
+        <Switch state={state} onChangeCheck={onChangeCheck} />
+      </div>
     </div>
   );
-}
+};
 
 export default Menu;
