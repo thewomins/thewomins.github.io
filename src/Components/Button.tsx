c60c0082987871ae575943760cb510c58873c69a
Felipe Urrutia <thewomins@gmail.com>
2022-10-17 19:13:11 -0300

    Fix format

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -11,7 +11,9 @@ type props = {
 const Button: React.FC<props> = ({primary = false, onClick, text, icon}) => {
   return (
     <div
-      className={`${styles.button} ${primary ? styles.primary : styles.secundary}`}
+      className={`${styles.button} ${
+        primary ? styles.primary : styles.secundary
+      }`}
       onClick={() => onClick()}
     >
       <div className={styles.image}>{icon}</div>
