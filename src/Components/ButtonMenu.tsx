159126584244796290b5bfeced0faa9cd9f6ab47
Felipe Urrutia <thewomins@gmail.com>
2022-10-17 19:15:18 -0300

    Adds button menu component

---
+++ src/Components/ButtonMenu.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./ButtonMenu.module.css";
+
+import {Link} from "react-scroll";
+
+type props = {
+  text: string;
+  to: string;
+};
+
+const ButtonMenu: React.FC<props> = ({to, text}) => {
+  return (
+    <Link
+      to={to}
+      offset={-50}
+      spy={true}
+      smooth={true}
+      className={styles.button}
+    >
+      <p>{text}</p>
+    </Link>
+  );
+};
+
+export default ButtonMenu;
