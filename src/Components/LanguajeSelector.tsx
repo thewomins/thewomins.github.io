72cfca83218418c53e0058b240c7ca85bdd5b2a5
Felipe Urrutia <thewomins@gmail.com>
2022-11-05 16:43:50 -0300

    Replace functionality with hook language

--- src/Components/LanguajeSelector.tsx
+++ src/Components/LanguajeSelector.tsx
@@ -1,29 +1,26 @@
 import React from "react";
+import {useLanguage} from "src/hooks/Language";
 import {Languages} from "src/types/types";
 import styles from "./LanguajeSelector.module.css";
 
 type props = {
   languageList?: Languages[];
   language: Languages;
-  setSelectedLanguage: (leng: Languages) => void;
 };
 
-const LanguajeSelector: React.FC<props> = ({
-  languageList,
-  setSelectedLanguage,
-  language,
-}) => {
+const LanguajeSelector: React.FC<props> = ({languageList, language}) => {
+  const {dispatchLanguage} = useLanguage();
+
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedLanguage(e.target.value as Languages);
+    dispatchLanguage({
+      type: "setLanguage",
+      Language: e.target.value as Languages,
+    });
   };
 
   return (
     <div className={styles.languaje}>
-      <select
-        id="Languages"
-        defaultValue={language}
-        onChange={(e) => handleSelect(e)}
-      >
+      <select id="Languages" value={language} onChange={(e) => handleSelect(e)}>
         {languageList?.map((lenguaje, index) => (
           <option key={index} value={lenguaje}>
             {lenguaje}
